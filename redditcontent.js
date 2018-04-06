d1c99e64bf44d2a599a57321b49f9457d882a608
Alexander Robertson <alexrson@gmail.com>
2018-04-05 23:04:27 -0700

    initial commit

---
+++ redditcontent.js
@@ -0,0 +1,10 @@
+//Hide Ads in newsfeed
+function clearsignup() {
+  
+  document.getElementsByClassName("skip-for-now")[0].click();
+}
+clearsignup()
+//document.onload = clearsignup;
+//window.setInterval(function(){
+//      clearsignup();
+//}, 20000);
